1394944609b90a7ac13bbb21a7fc8e5182b66ffb
coleramos425 <colramos@amd.com>
2025-07-27 13:18:46 -0500

    More debugging of first dispatch
    Signed-off-by: coleramos425 <colramos@amd.com>

--- src/memory_analysis_handler.cc
+++ src/memory_analysis_handler.cc
@@ -746,6 +746,7 @@ void memory_analysis_handler_t::report_json() {
   
   std::cout << "has_data: " << has_data << std::endl;
   std::cout << "dispatch_id_: " << dispatch_id_ << std::endl;
+  std::cout << "is_first_dispatch: " << is_first_dispatch << std::endl;
   // Write opening bracket for first dispatch (always for JSON format)
   // Also handle case where dispatch_id_ is uninitialized (0) but we have data
   if (is_first_dispatch || (dispatch_id_ == 0 && has_data)) {
